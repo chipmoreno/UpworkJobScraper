1be1b3bfb6516e05dda22c5c59e9aac36c30d149
chipmoreno <chipmoreno@Chips-MacBook-Air.local>
2024-12-23 11:51:25 -0600

    Now returning slightly cleaner/more usable values...

--- app.py
+++ app.py
@@ -10,8 +10,23 @@ def index():
 
 @app.route('/jobs')
 def jobs():
+    job_list = []
     job_data = scraper()  # Call the scrape function from scraper.py
-    return jsonify(job_data)
+    #return jsonify(job_data)
+    for job in job_data.values():
+        title = job.get('title')
+        time_posted = job.get('time_posted')
+        description = job.get('description')[:100]  # Get the first 100 characters
+        link = job.get('link')
+
+        job_list.append({
+                'title': title,
+                'time_posted': time_posted,
+                'description': description,
+                'link': link
+            })
+        
+    return job_list
 
 if __name__ == '__main__':
     app.run(debug=True)
